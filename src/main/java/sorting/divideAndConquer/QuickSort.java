19d1ff54c6d571238a0a156e389fbf22e1d80d7a
Lívia Cavalcanti <livia.juliao@ccc.ufcg.edu.br>
2017-05-29 20:44:16 -0300

    acho q tah feito

--- src/main/java/sorting/divideAndConquer/QuickSort.java
+++ src/main/java/sorting/divideAndConquer/QuickSort.java
@@ -1,6 +1,9 @@
 package sorting.divideAndConquer;
 
+import java.util.Arrays;
+
 import sorting.AbstractSorting;
+import util.Util;
 
 /**
  * Quicksort is based on the divide-and-conquer paradigm. The algorithm chooses
@@ -12,12 +15,35 @@ import sorting.AbstractSorting;
  */
 public class QuickSort<T extends Comparable<T>> extends AbstractSorting<T> {
 
-   @Override
-   public void sort(T[] array, int leftIndex, int rightIndex) {
-	   int j;
-	   if (leftIndex > rightIndex) {
-		   j
-	   }
-   }
+	public static void main(String[] args) {
+		QuickSort<Integer> s = new QuickSort<Integer>();
+		Integer[] array = { 30, 28, 7, 29, 11};
+		System.out.println(array.length-1);
+		s.sort(array, 0, array.length-1);
+		System.out.println(Arrays.toString(array));
+	}
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+		int j;
+		if (leftIndex < rightIndex) {
+			j = quick(array, leftIndex, rightIndex);
+			sort(array, leftIndex, j);
+			sort(array, j + 1, rightIndex);
+		}
+	}
+
+	private int quick(T[] array, int comeco, int fim) {
+		int indicePivot = comeco;
+
+		for (int i = comeco + 1; i <= fim; i++) {
+			if (array[i].compareTo(array[indicePivot]) == -1) {
+				Util.swap(array, indicePivot, i);
+				indicePivot = indicePivot + 1;
+			}
+		}
+
+		return indicePivot;
+	}
 
 }
--- src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
+++ src/main/java/sorting/divideAndConquer/threeWayQuicksort/ThreeWayQuickSort.java
@@ -1,6 +1,7 @@
 package sorting.divideAndConquer.threeWayQuicksort;
 
 import sorting.AbstractSorting;
+import util.Util;
 
 public class ThreeWayQuickSort<T extends Comparable<T>> extends
 		AbstractSorting<T> {
@@ -11,7 +12,7 @@ public class ThreeWayQuickSort<T extends Comparable<T>> extends
 	 * e os maiores a direita do pivot, e depois aplicamos a mesma estrategia 
 	 * recursivamente na particao a esquerda do pivot e na particao a direita do pivot. 
 	 * 
-	 * Considerando um array com muitoe elementos repetidos, a estrategia do quicksort 
+	 * Considerando um array com muitos elementos repetidos, a estrategia do quicksort 
 	 * pode ser otimizada para lidar de forma mais eficiente com isso. Essa melhoria 
 	 * eh conhecida como quicksort tree way e consiste da seguinte ideia:
 	 * - selecione o pivot e particione o array de forma que
@@ -25,8 +26,51 @@ public class ThreeWayQuickSort<T extends Comparable<T>> extends
 	 **/
 	@Override
 	public void sort(T[] array, int leftIndex, int rightIndex) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Not implemented yet!");
+		if (leftIndex < rightIndex) {
+			int iguais = leftIndex;
+			int indicePivot = leftIndex;
+			
+			for (int i = leftIndex + 1; i <= rightIndex; i++) {
+				if (array[i].compareTo(array[indicePivot]) == 0) {
+					Util.swap(array, iguais + 1, i);
+					iguais = iguais + 1;
+				} else if (array[i].compareTo(array[indicePivot]) == -1) {
+					if (iguais == indicePivot)
+						iguais = indicePivot + 1;
+					else
+						iguais = iguais + 1;
+					Util.swap(array, indicePivot, i);
+					indicePivot = indicePivot + 1;
+
+				}
+			}
+			sort(array, leftIndex, indicePivot - 1);
+			sort(array, iguais + 1, rightIndex);
+		}
 	}
+
+//	private int quick(T[] array, int leftIndex, int rightIndex) {
+//
+//		int iguais = leftIndex;
+//		int indicePivot = leftIndex;
+//		
+//		for (int i = comeco + 1; i <= fim; i++) {
+//			if (array[i] == array[indicePivot]) {
+//				swap(array, iguais + 1, i);
+//				iguais = iguais + 1;
+//			} else if (array[i] < array[indicePivot]) {
+//				if (iguais == indicePivot)
+//					iguais = indicePivot + 1;
+//				else
+//					iguais = iguais + 1;
+//				swap(array, indicePivot, i);
+//				indicePivot = indicePivot + 1;
+//
+//			}
+//		}
+//		quickSort(array, comeco, indicePivot - 1);
+//		quickSort(array, iguais + 1, fim);
+//	}
+//	
 
 }
